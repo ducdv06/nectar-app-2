8e6d0053447dc15ccc5438474ec4d10679db1f7f
ducdv06 <dovanduc141005@gmail.com>
2026-03-14 10:03:19 +0700

    fix

---
+++ myApp/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View } from 'react-native';
+import SquareBox from './components/SquareBox';
+
+export default function App() {
+  return (
+    <View style={{ flex: 1 }}>
+      <SquareBox />
+    </View>
+  );
+}
